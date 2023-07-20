fdb9c57c476fb0223a0d6afd8170b497268eb897
Nicomend11 <nicolas.j.mendoza@gmail.com>
2023-07-19 21:13:08 -0300

    Tipo de Datos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,56 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+      System.out.println("Hello and welcome!"/n);
 
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+       System.out.println("chauuu");
+
+       System.out.println(12334);
+
+       System.out.println(123.556);
+
+       System.out.println('c');
+
+       System.out.println(true);
+
+       System.out.println(false);
+
+        // declaracion e instanciacion
+        int edad; // declarando la variable edad de tipo int
+        char fecha;
+        edad = 18; // inicializando la variable edad con valor 18
+        char tipo_documento = 'D'; // declara e inicializar una variable
+
+        // tipos primitivos
+        byte var1 = 5;
+        short var2 = 100;
+        int var3 = 10000;
+        long var4 = 100000000000000000L;
+
+        System.out.println(var1 + var3);
+
+        float var5 = 10.12f;
+        double var6 = 10.2222222222222;
+
+        char var7 = 't';
+
+        boolean var8 = true;
+        boolean var9 = false;
+
+        String var10 = "hola"; // va a ser una referencia
+
+        int valor1 = 10;
+        int valor2 = 20;
+
+        int suma = valor1 + valor2;
+        System.out.println(suma);
+
+        System.out.println(var5 + var6);
+
+        System.out.println(var3 + var5);
+
+
     }
 }

713e67c988b8fda6a9d6ab2383092cf7c9033163
Nicomend11 <nicolas.j.mendoza@gmail.com>
2023-09-22 22:37:40 -0300

    Actualizado con TP Final

--- src/main/java/org/example/ClaseAutomation/OpenCart/CartPage.java
+++ src/main/java/org/example/ClaseAutomation/OpenCart/CartPage.java
@@ -5,15 +5,15 @@ import org.openqa.selenium.WebDriver;
 
 public class CartPage extends Page {
 
-    private By selectProduct = By.xpath("//a[contains(@href, 'product_id=30') and contains(@href, 'route=product/product') and contains(@href, 'path=33') and text()='Canon EOS 5D']");
-    private By nameCamera = By.xpath("//a[contains(@href, 'product_id=30') and text()='Canon EOS 5D']");
-    private By selectOptions = By.xpath("//select[@name='option[226]' and @id='input-option226']");
+    private By selectProduct = By.xpath("//a[contains(@href, 'product_id=30') and text()='Canon EOS 5D']");
+    private By nameCamera = By.xpath("//h1[text()='Canon EOS 5D']");
+    private By selectOptions = By.xpath("//select[@name='option[226]']");
     private By selectColor = By.xpath("//option[@value='16']");
-    private By addToCartBtn = By.xpath("//button[@id='button-cart' and text()='Add to Cart']");
-    private By shoppingCartBtn = By.xpath ("//button[contains(@class, 'btn-inverse') and contains(@class, 'btn-block') and contains(@class, 'btn-lg') and contains(@class, 'dropdown-toggle')]");
-    private By viewCartBtn = By.xpath("//strong[contains(text(), 'View Cart') and i[@class='fa fa-shopping-cart']]");
-    private By existsProduct = By.xpath("//td[@class='text-left' and a[contains(@href, 'product_id=30')] and small[text()='Select: Blue'] and small[contains(text(), 'Reward Points: 200')]]");
-    private By shoppCartBtn = By.xpath("//a[contains(@href, 'route=checkout/cart') and contains(@title, 'Shopping Cart') and i[@class='fa fa-shopping-cart'] and span[@class='hidden-xs hidden-sm hidden-md' and text()='Shopping Cart']]");
+    private By addToCartBtn = By.xpath("//button[@id='button-cart']");
+    private By shoppingCartBtn = By.xpath ("//button[contains(@class, 'btn-inverse')]");
+    private By viewCartBtn = By.xpath("//strong[contains(text(), 'View Cart')]");
+    private By existsProduct = By.xpath("//td[@class='text-left' and text()='Product 3']");
+    private By shoppCartBtn = By.xpath("//a[contains(@href, 'route=checkout/cart') and contains(@title, 'Shopping Cart')]");
     private By removeBtn = By.xpath("//i[@class='fa fa-times-circle']");
     private By emptyMessage = By.xpath("//p[text()='Your shopping cart is empty!']");
     public CartPage(WebDriver driver) {
